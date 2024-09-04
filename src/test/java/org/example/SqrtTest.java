8d8e9526607716a63d378ce19c2fc7fdfb24f5d2
PavelLSmirnov <pavel1201s@yandex.ru>
2024-09-04 13:27:42 +0300

    feat(tests): create tests

--- src/test/java/org/example/SqrtTest.java
+++ src/test/java/org/example/SqrtTest.java
@@ -9,14 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SqrtTest {
     @Test
     public void testAverage() {
-        Sqrt sqrt = new Sqrt(2.0);
-        double result = sqrt.average(2.0, 4.0);
-        assertEquals(3.0, result, 1e-9);
+        Sqrt sqrt = new Sqrt(0);
+        double result = sqrt.average(4.0, 4.0);
+        assertEquals(result, 4.0);
     }
 
     @Test
     public void testGood() {
-        Sqrt sqrt = new Sqrt(2.0);
+        Sqrt sqrt = new Sqrt(0);
         assertTrue(sqrt.good(1.41421356, 2.0));
         assertFalse(sqrt.good(1.0, 2.0));
     }
@@ -25,20 +25,20 @@ public class SqrtTest {
     public void testImprove() {
         Sqrt sqrt = new Sqrt(2.0);
         double result = sqrt.improve(1.0, 2.0);
-        assertEquals(1.5, result, 1e-9);
+        assertEquals(1.5, result);
     }
 
     @Test
     public void testIter() {
         Sqrt sqrt = new Sqrt(2.0);
         double result = sqrt.iter(1.0, 2.0);
-        assertEquals(1.41421356, result, 1e-8);
+        assertEquals(1.41421356, result);
     }
 
     @Test
     public void testCalc() {
         Sqrt sqrt = new Sqrt(2.0);
         double result = sqrt.calc();
-        assertEquals(1.41421356, result, 1e-8);
+        assertEquals(1.41421356, result);
     }
 }
